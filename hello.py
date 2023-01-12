bf1891f1afedacfeff832ba6b1a4e2b84e93b89e
Mhia Mojica <mhiamojica@gmail.com>
2023-01-11 16:53:16 -0800

    URL building

--- hello.py
+++ hello.py
@@ -2,13 +2,21 @@
 
 from markupsafe import escape
 
-@app.route("/<name>")
-def hello(name):
-    return f"hello, {escape(name)}!"
+@app.route("/user/<username>")
+def show_user_profile(username)::
+    return f'User {escape(username)}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return f'Post {post_id}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    return f'Subpath {escape(subpath)}'
 
 """
 
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
@@ -19,4 +27,18 @@ def index():
 @app.route('/hello')
 def hello():
     return 'Hello, World'
+
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
 
